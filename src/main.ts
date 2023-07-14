4d3c7c69c816915f06295a42d9c74578fe588110
dorinaklung <dorina.klung@gmail.com>
2023-07-14 20:46:45 +0200

    Adding buttons TS 1

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+const logNumberToConsole = (event: MouseEvent) => {
+  console.log(event.target.innerHTML);
+}
 
+const addNumberButtonEventListeners = () => {
+  for (let i = 0; i < 10; i++) {
+    const buttonElement = document.getElementById(`button-${i}`);
+
+    if (buttonElement) {
+      buttonElement.addEventListener('click', logNumberToConsole);
+    } 
+  }
+}
+
+addNumberButtonEventListeners();
